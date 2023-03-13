3131396c21998728227e4957b04cbd7f2cc3b3fd
indamutsa <arsichizy@gmail.com>
2023-03-13 15:40:24 +0100

    added credentials

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -16,6 +16,6 @@ terraform {
 provider "aws" {
   region                   = "us-east-1"
   shared_credentials_files = ["./terraform/credentials"]
-  profile                  = "indamutsa"
+  # profile                  = "indamutsa"
 }
 
